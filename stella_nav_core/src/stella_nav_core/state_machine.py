74e00ca38afce5df70d157cdf54eeff063ee447f
ymd-stella <world.applepie@gmail.com>
2019-07-16 22:51:22 +0900

    Use GraphMachine

--- stella_nav_core/src/stella_nav_core/state_machine.py
+++ stella_nav_core/src/stella_nav_core/state_machine.py
@@ -1,13 +1,15 @@
-from transitions import Machine
+from transitions.extensions import GraphMachine
 
 
 class StateMachine(object):
-    def __init__(self, states, transitions, initial=None):
+    def __init__(self, states, transitions, draw_file="/tmp/state_machine.png", draw_prog="dot", initial=None):
         if initial is None:
             initial = states[0]
-        self._machine = Machine(
+        self._machine = GraphMachine(
             model=self,
             states=states,
             transitions=transitions,
             initial=initial,
             ignore_invalid_triggers=True)
+        if draw_file:
+            self._machine.get_graph().draw(draw_file, prog=draw_prog)
