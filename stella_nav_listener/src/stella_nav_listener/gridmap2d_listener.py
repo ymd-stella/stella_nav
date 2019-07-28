b92f2dcaf52d0bb03c9ea4228124dc3444af2681
ymd-stella <world.applepie@gmail.com>
2019-07-28 14:16:31 +0900

    Update gridmap2d_listener
    Add visualization_scale to reduce the cpu load of serialization

--- stella_nav_listener/src/stella_nav_listener/gridmap2d_listener.py
+++ stella_nav_listener/src/stella_nav_listener/gridmap2d_listener.py
@@ -2,20 +2,24 @@ import rospy
 from nav_msgs.msg import OccupancyGrid, MapMetaData
 from geometry_msgs.msg import Pose, Point, Quaternion
 import numpy as np
+import cv2
 
 class GridMap2DListener(object):
-    def __init__(self, topic, handler, handlers, **kwargs):
+    def __init__(self, topic, handler, handlers, visualization_scale=5, **kwargs):
         super(GridMap2DListener, self).__init__()
         self._handler = handlers[handler]
         self._pub = rospy.Publisher(topic, OccupancyGrid, queue_size=1)
         self._costmap = None
+        self._visualization_scale = visualization_scale
 
     def __call__(self, msg):
         costmap = self._handler.costmap()
         if costmap is not None:
             cells = costmap.cells
-            origin = costmap.origin
-            resolution = costmap.resolution
+            rows, cols = cells.shape
+            cells = cv2.resize(cells, (rows/self._visualization_scale, cols/self._visualization_scale))
+            origin = costmap.origin / self._visualization_scale
+            resolution = costmap.resolution * self._visualization_scale
             msg = OccupancyGrid()
             msg.header.frame_id = "map"
             msg.header.stamp = costmap.stamp
